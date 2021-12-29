4bd2870eb1c475af05fd7c14ca0761480b90f939
teyouale <teyouale@gmail.com>
2021-12-29 11:09:12 +0300

    Fixing Route Problem for Dashboard Catagories

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -2,24 +2,24 @@ import React from "react";
 import Main from "../../Layouts/Main";
 import DashboardCard from "../../Components/DashboardCard";
 const Dashboard = () => {
-  return (
-    <div>
-      <Main>
-        <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-wrap -m-4">
-              <DashboardCard />
-              <DashboardCard />
-              <DashboardCard />
-              <DashboardCard />
-              <DashboardCard />
-              <DashboardCard />
-            </div>
-          </div>
-        </section>
-      </Main>
-    </div>
-  );
+	return (
+		<div>
+			<Main>
+				<section class='text-gray-600 body-font'>
+					<div class='container px-5 py-24 mx-auto'>
+						<div class='flex flex-wrap -m-4'>
+							<DashboardCard />
+							<DashboardCard />
+							<DashboardCard />
+							<DashboardCard />
+							<DashboardCard />
+							<DashboardCard />
+						</div>
+					</div>
+				</section>
+			</Main>
+		</div>
+	);
 };
 
 export default Dashboard;

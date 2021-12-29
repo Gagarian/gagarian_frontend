995d1fdba818c6d3fdf434cf40cc48c856247f62
natilemma <natilemma5@gmail.com>
2021-12-29 10:17:23 +0300

    <DashboardCard />               <DashboardCard />               <DashboardCard />             </div>           </div>         </section>       </Main>     </div>   ); }; dashboard card added

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Main from "../../Layouts/Main";
+import DashboardCard from "../../Components/DashboardCard";
 const Dashboard = () => {
-	return (
-		<div>
-			<Main>Dashboard</Main>
-		</div>
-	);
+  return (
+    <div>
+      <Main>
+        <section class="text-gray-600 body-font">
+          <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap -m-4">
+              <DashboardCard />
+              <DashboardCard />
+              <DashboardCard />
+              <DashboardCard />
+              <DashboardCard />
+              <DashboardCard />
+            </div>
+          </div>
+        </section>
+      </Main>
+    </div>
+  );
 };
 
 export default Dashboard;

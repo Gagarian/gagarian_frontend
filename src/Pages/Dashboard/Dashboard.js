23d9735b3b7859547932606fdd09aef1c0fa443c
teyouale <teyouale@gmail.com>
2021-12-29 20:39:26 +0300

    configuring redux

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Main from "../../Layouts/Main";
 import DashboardCard from "../../Components/DashboardCard";
+import { useSelector, useDispatch } from "react-redux";
+import { retrievecatagories } from "../../Services/Slices/catagoriesSlice";
 const Dashboard = () => {
+	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(retrievecatagories());
+		// console.log(x);
+	}, [dispatch]);
 	return (
 		<div>
 			<Main>

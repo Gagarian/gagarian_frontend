0f38b7b78f9e6a5d1878a5cab52a8eb07a4bc61e
teyouale <teyouale@gmail.com>
2021-12-30 11:44:49 +0300

    Modifing Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,91 +1,201 @@
-import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
 const Header = () => {
-	let navigate = useNavigate();
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	return (
-		<nav class='flex items-center justify-between flex-wrap bg-white py-4 lg:px-12 shadow border-solid border-t-2 border-blue-700'>
-			<div class='flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0'>
-				<div
-					class='flex items-center flex-shrink-0 text-gray-800 mr-16 cursor-pointer'
-					onClick={() => navigate(`/`)}>
-					<span class='font-semibold text-xl tracking-tight'>My Navbar</span>
-				</div>
-				<div class='block lg:hidden '>
+		<div class='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+			<div class='relative flex items-center justify-between'>
+				<a
+					href='/'
+					aria-label='Company'
+					title='Company'
+					class='inline-flex items-center'>
+					<svg
+						class='w-8 text-deep-purple-accent-400'
+						viewBox='0 0 24 24'
+						strokeLinejoin='round'
+						strokeWidth='2'
+						strokeLinecap='round'
+						strokeMiterlimit='10'
+						stroke='currentColor'
+						fill='none'>
+						<rect x='3' y='1' width='7' height='12' />
+						<rect x='3' y='17' width='7' height='6' />
+						<rect x='14' y='1' width='7' height='6' />
+						<rect x='14' y='11' width='7' height='12' />
+					</svg>
+					<span class='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+						Company
+					</span>
+				</a>
+				<ul class='flex items-center hidden space-x-8 lg:flex'>
+					<li>
+						<a
+							href='/'
+							aria-label='Our product'
+							title='Our product'
+							class='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'>
+							Product
+						</a>
+					</li>
+					<li>
+						<a
+							href='/'
+							aria-label='Our product'
+							title='Our product'
+							class='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'>
+							Features
+						</a>
+					</li>
+					<li>
+						<a
+							href='/'
+							aria-label='Product pricing'
+							title='Product pricing'
+							class='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'>
+							Pricing
+						</a>
+					</li>
+					<li>
+						<a
+							href='/'
+							aria-label='About us'
+							title='About us'
+							class='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'>
+							About us
+						</a>
+					</li>
+					<li>
+						<a
+							href='/login'
+							class='inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none'
+							aria-label='Sign in'
+							title='Sign in'>
+							Sign IN
+						</a>
+					</li>
+				</ul>
+				<div class='lg:hidden'>
 					<button
-						id='nav'
-						class='flex items-center px-3 py-2 border-2 rounded text-blue-700 border-blue-700 hover:text-blue-700 hover:border-blue-700'>
-						<svg
-							class='fill-current h-3 w-3'
-							viewBox='0 0 20 20'
-							xmlns='http://www.w3.org/2000/svg'>
-							<title>Menu</title>
-							<path d='M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z' />
+						aria-label='Open Menu'
+						title='Open Menu'
+						class='p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50'
+						onClick={() => setIsMenuOpen(true)}>
+						<svg class='w-5 text-gray-600' viewBox='0 0 24 24'>
+							<path
+								fill='currentColor'
+								d='M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z'
+							/>
+							<path
+								fill='currentColor'
+								d='M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z'
+							/>
+							<path
+								fill='currentColor'
+								d='M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z'
+							/>
 						</svg>
 					</button>
+					{isMenuOpen && (
+						<div class='absolute top-0 left-0 w-full'>
+							<div class='p-5 bg-white border rounded shadow-sm'>
+								<div class='flex items-center justify-between mb-4'>
+									<div>
+										<a
+											href='/'
+											aria-label='Company'
+											title='Company'
+											class='inline-flex items-center'>
+											<svg
+												class='w-8 text-deep-purple-accent-400'
+												viewBox='0 0 24 24'
+												strokeLinejoin='round'
+												strokeWidth='2'
+												strokeLinecap='round'
+												strokeMiterlimit='10'
+												stroke='currentColor'
+												fill='none'>
+												<rect x='3' y='1' width='7' height='12' />
+												<rect x='3' y='17' width='7' height='6' />
+												<rect x='14' y='1' width='7' height='6' />
+												<rect x='14' y='11' width='7' height='12' />
+											</svg>
+											<span class='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+												Company
+											</span>
+										</a>
+									</div>
+									<div>
+										<button
+											aria-label='Close Menu'
+											title='Close Menu'
+											class='p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline'
+											onClick={() => setIsMenuOpen(false)}>
+											<svg class='w-5 text-gray-600' viewBox='0 0 24 24'>
+												<path
+													fill='currentColor'
+													d='M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z'
+												/>
+											</svg>
+										</button>
+									</div>
+								</div>
+								<nav>
+									<ul class='space-y-4'>
+										<li>
+											<a
+												href='/'
+												aria-label='Our product'
+												title='Our product'
+												class='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'>
+												Product
+											</a>
+										</li>
+										<li>
+											<a
+												href='/'
+												aria-label='Our product'
+												title='Our product'
+												class='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'>
+												Features
+											</a>
+										</li>
+										<li>
+											<a
+												href='/'
+												aria-label='Product pricing'
+												title='Product pricing'
+												class='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'>
+												Pricing
+											</a>
+										</li>
+										<li>
+											<a
+												href='/'
+												aria-label='About us'
+												title='About us'
+												class='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'>
+												About us
+											</a>
+										</li>
+										<li>
+											<a
+												href='/'
+												class='inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none'
+												aria-label='Sign in'
+												title='Sign in'>
+												Sign in
+											</a>
+										</li>
+									</ul>
+								</nav>
+							</div>
+						</div>
+					)}
 				</div>
 			</div>
-
-			<div class='menu w-full lg:block flex-grow lg:flex lg:items-center lg:w-auto lg:px-3 px-8'>
-				<div class='text-md font-bold text-blue-700 lg:flex-grow'>
-					<a
-						href='#responsive-header'
-						class='block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2'>
-						Menu 1
-					</a>
-					<a
-						href='#responsive-header'
-						class=' block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2'>
-						Menu 2
-					</a>
-					<a
-						href='#responsive-header'
-						class='block mt-4 lg:inline-block lg:mt-0 hover:text-white px-4 py-2 rounded hover:bg-blue-700 mr-2'>
-						Menu 3
-					</a>
-				</div>
-				<div class='relative mx-auto text-gray-600 lg:block hidden'>
-					<input
-						class='border-2 border-gray-300 bg-white h-10 pl-2 pr-8 rounded-lg text-sm focus:outline-none'
-						type='search'
-						name='search'
-						placeholder='Search'
-					/>
-					<button type='submit' class='absolute right-0 top-0 mt-3 mr-2'>
-						<svg
-							class='text-gray-600 h-4 w-4 fill-current'
-							xmlns='http://www.w3.org/2000/svg'
-							version='1.1'
-							id='Capa_1'
-							x='0px'
-							y='0px'
-							viewBox='0 0 56.966 56.966'
-							style={{ enableBackground: "new 0 0 56.966 56.966" }}
-							// xml:upace='preserve'
-							width='512px'
-							height='512px'>
-							<path d='M55.146,51.887L41.588,37.786c3.486-4.144,5.396-9.358,5.396-14.786c0-12.682-10.318-23-23-23s-23,10.318-23,23  s10.318,23,23,23c4.761,0,9.298-1.436,13.177-4.162l13.661,14.208c0.571,0.593,1.339,0.92,2.162,0.92  c0.779,0,1.518-0.297,2.079-0.837C56.255,54.982,56.293,53.08,55.146,51.887z M23.984,6c9.374,0,17,7.626,17,17s-7.626,17-17,17  s-17-7.626-17-17S14.61,6,23.984,6z' />
-						</svg>
-					</button>
-				</div>
-				<div class='flex '>
-					<a
-						href='#'
-						class='block text-md px-4 py-2 rounded text-blue-700 ml-2 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0'>
-						<Link to='/signup'>Sign Up</Link>
-					</a>
-
-					<a
-						href='#'
-						class=' block text-md px-4  ml-2 py-2 rounded text-blue-700 font-bold hover:text-white mt-4 hover:bg-blue-700 lg:mt-0'>
-						<Link to='/login'>Login</Link>
-					</a>
-				</div>
-			</div>
-		</nav>
+		</div>
 	);
 };
-
 export default Header;

8ec5c0972518b8db5b45e4e2ea9b567b3a66eb2b
teyouale <teyouale@gmail.com>
2022-01-01 01:24:29 +0300

    Removing Product from Persiste State

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -4,34 +4,35 @@ import DashboardCard from "../../Components/DashboardCard";
 import Carousel from "../../Components/Carousel";
 import { useSelector, useDispatch } from "react-redux";
 import {
-  getProducts,
-  clearState,
-  productSelector,
+	getProducts,
+	clearState,
+	productSelector,
 } from "../../Services/Slices/productSlice";
 const Dashboard = ({ toggle, handleToggle }) => {
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  const { result } = useSelector(productSelector);
-  useEffect(() => {
-    dispatch(getProducts());
-    console.log(result);
-  }, [dispatch]);
-  return (
-    <div>
-      <Main toggle={toggle} handleToggle={handleToggle}>
-        <section class="text-gray-600 body-font">
-          <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-wrap -m-4">
-              <Carousel />
-              {result.map((item, index) => (
-                <DashboardCard key={index} item={item} />
-              ))}{" "}
-            </div>
-          </div>
-        </section>
-      </Main>
-    </div>
-  );
+	const { result } = useSelector(productSelector);
+	useEffect(() => {
+		dispatch(getProducts());
+		// console.log(result);
+	}, [dispatch]);
+	return (
+		<div>
+			<Main toggle={toggle} handleToggle={handleToggle}>
+				<section class='text-gray-600 body-font'>
+					<div class='container px-5 py-24 mx-auto'>
+						<div class='flex flex-wrap -m-4'>
+							{result !== null
+								? result.map((item, index) => (
+										<DashboardCard key={index} item={item} />
+								  ))
+								: ""}
+						</div>
+					</div>
+				</section>
+			</Main>
+		</div>
+	);
 };
 
 export default Dashboard;

ddb344e1ecf1177ea50867cf0a26f87986906dc5
teyouale <teyouale@gmail.com>
2022-01-12 22:32:18 +0300

    Add Google Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
 		extend: {
 			fontFamily: {
 				// sans: ["Montserrat"],
+				sans: ["Prompt", ...defaultTheme.fontFamily.sans],
 			},
 			colors: {
 				red: {

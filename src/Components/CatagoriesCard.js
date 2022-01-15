0a3297b52406926f81130741d0d7b3245752637d
natilemma <natilemma5@gmail.com>
2022-01-15 16:45:47 +0300

    modefied the category list page

--- src/Components/CatagoriesCard.js
+++ src/Components/CatagoriesCard.js
@@ -1,50 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const CatagoriesCard = () => {
+const CatagoriesCard = ({ item }) => {
   let navigate = useNavigate();
   return (
-    <section class="text-gray-600 body-font overflow-hidden">
-      <div class="container px-5 py-24 mx-auto">
-        <div class="-my-8 divide-y-2 divide-gray-100">
-          <div class="py-8 flex flex-wrap md:flex-nowrap">
-            <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-              <img
-                alt="ecommerce"
-                className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-                src="https://media.istockphoto.com/photos/injera-a-staple-food-of-ethiopia-picture-id1131817698?k=20&m=1131817698&s=612x612&w=0&h=N-CCH8dg8uEFDdDW_h_oCSFpbWz0DHscqukukBIaNS0="
-              />
-            </div>
-            <div class="md:flex-grow">
-              <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
-                Bitters hashtag waistcoat fashion axe chia unicorn
-              </h2>
-              <p class="leading-relaxed">
-                Glossier echo park pug, church-key sartorial biodiesel
-                vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf moon
-                party messenger bag selfies, poke vaporware kombucha
-                lumbersexual pork belly polaroid hoodie portland craft beer.
-              </p>
-              <a class="text-indigo-500 inline-flex items-center mt-4">
-                Learn More
-                <svg
-                  class="w-4 h-4 ml-2"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path d="M5 12h14"></path>
-                  <path d="M12 5l7 7-7 7"></path>
-                </svg>
-              </a>
-            </div>
-          </div>
-        </div>
+    <div
+      class="lg:w-1/4 md:w-1/2 py-10 px-5 w-full inline-block"
+      onClick={() => navigate(`/item/${item.id}`)}
+    >
+      <a class="block relative h-48 rounded overflow-hidden">
+        <img
+          alt="ecommerce"
+          class="object-cover object-center w-full h-full block"
+          src="https://dummyimage.com/420x260"
+        />
+      </a>
+      <div class="mt-4">
+        <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">
+          CATEGORY
+        </h3>
+        <h2 class="text-gray-900 title-font text-lg font-medium">
+          The Catalyzer
+        </h2>
+        <p class="mt-1">$16.00</p>
       </div>
-    </section>
+    </div>
   );
 };
 

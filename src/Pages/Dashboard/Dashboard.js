248348736dde3ffd18a88013194247ab9e9992ce
teyouale <teyouale@gmail.com>
2022-01-15 16:28:44 +0300

    Working With Paginate

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -21,18 +21,17 @@ const Dashboard = ({ toggle, handleToggle }) => {
 		useSelector(productSelector);
 
 	let limit = 3;
-	// const [items, setItems] = useState([]);
 	let toastWaiting;
 	const [pageCount, setpageCount] = useState(0);
 
 	const handlePageClick = async (data) => {
 		console.log(data.selected);
-		// let currentPage = data.selected + 1;
+		let currentPage = data.selected + 1;
 		// const commentsFormServer = await fetchComments(currentPage);
-
+		fetchProducts(currentPage);
 		// setItems(commentsFormServer);
 		// scroll to the top
-		//window.scrollTo(0, 0)
+		// window.scrollTo(0, 0);
 	};
 	const fetchProducts = async (currentPage) => {
 		// const data = await res.json();
@@ -89,27 +88,26 @@ const Dashboard = ({ toggle, handleToggle }) => {
 						nextLabel={"next"}
 						breakLabel={"..."}
 						pageCount={pageCount}
-						marginPagesDisplayed={2}
-						pageRangeDisplayed={1}
+						marginPagesDisplayed={1}
+						pageRangeDisplayed={pageCount}
 						onPageChange={handlePageClick}
 						containerClassName={"flex"}
-						pageClassName={
+						pageLinkClassName={
 							"px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white"
 						}
-						// pageLinkClassName={"page-link"}
-						previousClassName={
-							"flex items-center px-4 py-2 mx-1 text-gray-500 bg-white rounded-md cursor-not-allowed dark:bg-gray-900 dark:text-gray-600"
-						}
-						// previousLinkClassName={"page-link"}
-						nextClassName={
+						previousLinkClassName={
 							"px-4 py-2 font-bold text-gray-500 bg-gray-300 rounded-md hover:bg-blue-400 hover:text-white"
 						}
-						// nextLinkClassName={"page-link"}
-						breakClassName={
+						// previousLinkClassName={"page-link"}
+						nextLinkClassName={
+							"px-4 py-2 font-bold text-gray-500 bg-gray-300 rounded-md hover:bg-blue-400 hover:text-white"
+						}
+						// nextLinkLinkClassName={"page-link"}
+						breakLinkClassName={
 							"px-4 py-2 text-gray-700 bg-gray-200 rounded-md hover:bg-blue-400 hover:text-white"
 						}
-						// breakLinkClassName={"page-link"}
-						activeClassName={" text-green-500"}
+						// breakLinkLinkClassName={"page-link"}
+						activeLinkClassName={" bg-green-500"}
 					/>
 				</section>
 			</Main>

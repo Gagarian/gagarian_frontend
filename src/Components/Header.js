4b6a34a1ee1ae5124b75d103f75315c7e9b02ec5
teyouale <teyouale@gmail.com>
2022-01-16 15:52:44 +0300

    Handle Toggle

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -27,6 +27,9 @@ const Header = ({ gone }) => {
 	const [top, setTop] = useState(true);
 	const [visiableGone, setvisiableGone] = useState(!top);
 	const visiableStyle = visiableGone ? "hidden" : "visible";
+	const [toggle, setToggle] = useState(true);
+
+	const toggleClass = " transform translate-x-5 bg-green-500";
 
 	const handleLogout = () => {
 		// TODO Save Token
@@ -50,6 +53,13 @@ const Header = ({ gone }) => {
 		return () => window.removeEventListener("scroll", scrollHandler);
 	}, [top]);
 
+	useEffect(() => {
+		if (toggle) {
+			handleClick("en");
+		} else {
+			handleClick("am");
+		}
+	}, [toggle]);
 	return (
 		// <nav className='bg-white shadow dark:bg-gray-800'><div class="bg-gray-900">
 		<header
@@ -96,64 +106,76 @@ const Header = ({ gone }) => {
 							</button>
 						</div>
 					</div>
-					{username != null ? (
-						<div className='flex items-center mt-4 md:mt-0'>
-							<button
-								onClick={() => navigate("/cart")}
-								className='hidden mx-4 text-gray-600 transition-colors duration-200 transform md:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none'
-								aria-label='show notifications'>
-								<svg
-									className='w-5 h-5'
-									viewBox='0 0 24 24'
-									fill='none'
-									xmlns='http://www.w3.org/2000/svg'>
-									<path
-										d='M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z'
-										stroke='currentColor'
-										strokeWidth='2'
-										strokeLinecap='round'
-										strokeLinejoin='round'
-									/>
-								</svg>
 
-								<span className='absolute top-0 left-0 p-1 text-xs text-white bg-blue-500 rounded-full'></span>
-							</button>
-
-							<button
-								type='button'
-								className='flex items-center focus:outline-none'
-								aria-label='toggle profile dropdown'>
-								<div className='w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full'>
-									<div class='dropdown dropdown-right dropdown-end'>
-										<img
-											tabindex='0'
-											src='https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
-											className='object-cover w-full h-full'
-											alt='avatar'
-										/>
-										<ul
-											tabindex='0'
-											class='p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52'>
-											<li>
-												<a>Item 1</a>
-											</li>
-											<li>
-												<a>Item 2</a>
-											</li>
-											<li>
-												<a>Item 3</a>
-											</li>
-										</ul>
-									</div>
-								</div>
-
-								<h3 className='mx-2 text-sm font-medium text-gray-700 dark:text-gray-200 md:hidden'>
-									{username}
-								</h3>
-							</button>
+					<div className='flex items-center mt-4 md:mt-0'>
+						<div
+							className='md:w-14 md:h-7 w-12 h-6 flex items-center bg-gray-300 rounded-full p-1 cursor-pointer'
+							onClick={() => {
+								setToggle(!toggle);
+							}}>
+							<div
+								className={
+									"bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform" +
+									(toggle ? null : toggleClass)
+								}></div>
 						</div>
-					) : (
-						<div className='flex items-center mt-4 md:mt-0'>
+						{username != null ? (
+							<>
+								<button
+									onClick={() => navigate("/cart")}
+									className='hidden mx-4 text-gray-600 transition-colors duration-200 transform md:block dark:text-gray-200 hover:text-gray-700 dark:hover:text-gray-400 focus:text-gray-700 dark:focus:text-gray-400 focus:outline-none'
+									aria-label='show notifications'>
+									<svg
+										className='w-5 h-5'
+										viewBox='0 0 24 24'
+										fill='none'
+										xmlns='http://www.w3.org/2000/svg'>
+										<path
+											d='M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z'
+											stroke='currentColor'
+											strokeWidth='2'
+											strokeLinecap='round'
+											strokeLinejoin='round'
+										/>
+									</svg>
+
+									<span className='absolute top-0 left-0 p-1 text-xs text-white bg-blue-500 rounded-full'></span>
+								</button>
+
+								<button
+									type='button'
+									className='flex items-center focus:outline-none'
+									aria-label='toggle profile dropdown'>
+									<div className='w-8 h-8 overflow-hidden border-2 border-gray-400 rounded-full'>
+										<div class='dropdown dropdown-right dropdown-end'>
+											<img
+												tabindex='0'
+												src='https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80'
+												className='object-cover w-full h-full'
+												alt='avatar'
+											/>
+											<ul
+												tabindex='0'
+												class='p-2 shadow menu dropdown-content bg-base-100 rounded-box w-52'>
+												<li>
+													<a>Item 1</a>
+												</li>
+												<li>
+													<a>Item 2</a>
+												</li>
+												<li>
+													<a>Item 3</a>
+												</li>
+											</ul>
+										</div>
+									</div>
+
+									<h3 className='mx-2 text-sm font-medium text-gray-700 dark:text-gray-200 md:hidden'>
+										{username}
+									</h3>
+								</button>
+							</>
+						) : (
 							<ul class='flex items-center hidden ml-auto space-x-8 lg:flex'>
 								<li>
 									<a
@@ -174,8 +196,8 @@ const Header = ({ gone }) => {
 									</a>
 								</li>
 							</ul>
-						</div>
-					)}
+						)}
+					</div>
 				</div>
 				{/* SubHeader */}
 
@@ -204,39 +226,6 @@ const Header = ({ gone }) => {
 							class='border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6'>
 							{t("Translate.4")}
 						</a>
-
-						<a
-							href='#'
-							className='border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6'>
-							<svg
-								className='w-4 h-4 fill-current'
-								viewBox='0 0 24 24'
-								fill='none'
-								xmlns='http://www.w3.org/2000/svg'>
-								<path
-									fillRule='evenodd'
-									clipRule='evenodd'
-									d='M1 11.9554V12.0446C1.01066 14.7301 1.98363 17.1885 3.59196 19.0931C4.05715 19.6439 4.57549 20.1485 5.13908 20.5987C5.70631 21.0519 6.31937 21.4501 6.97019 21.7853C7.90271 22.2656 8.91275 22.6165 9.97659 22.8143C10.5914 22.9286 11.2243 22.9918 11.8705 22.9993C11.9136 22.9998 11.9567 23 11.9999 23C15.6894 23 18.9547 21.1836 20.9502 18.3962C21.3681 17.8125 21.7303 17.1861 22.0291 16.525C22.6528 15.1448 22.9999 13.613 22.9999 12C22.9999 8.73978 21.5816 5.81084 19.3283 3.79653C18.8064 3.32998 18.2397 2.91249 17.6355 2.55132C15.9873 1.56615 14.0597 1 11.9999 1C11.888 1 11.7764 1.00167 11.6653 1.00499C9.99846 1.05479 8.42477 1.47541 7.0239 2.18719C6.07085 2.67144 5.19779 3.29045 4.42982 4.01914C3.7166 4.69587 3.09401 5.4672 2.58216 6.31302C2.22108 6.90969 1.91511 7.54343 1.6713 8.20718C1.24184 9.37631 1.00523 10.6386 1 11.9554ZM20.4812 15.0186C20.8171 14.075 20.9999 13.0588 20.9999 12C20.9999 9.54265 20.0151 7.31533 18.4186 5.6912C17.5975 7.05399 16.5148 8.18424 15.2668 9.0469C15.7351 10.2626 15.9886 11.5603 16.0045 12.8778C16.7692 13.0484 17.5274 13.304 18.2669 13.6488C19.0741 14.0252 19.8141 14.487 20.4812 15.0186ZM15.8413 14.8954C16.3752 15.0321 16.904 15.22 17.4217 15.4614C18.222 15.8346 18.9417 16.3105 19.5723 16.8661C18.0688 19.2008 15.5151 20.7953 12.5788 20.9817C13.5517 20.0585 14.3709 18.9405 14.972 17.6514C15.3909 16.7531 15.678 15.8272 15.8413 14.8954ZM13.9964 12.6219C13.9583 11.7382 13.7898 10.8684 13.5013 10.0408C10.6887 11.2998 7.36584 11.3765 4.35382 9.97197C4.01251 9.81281 3.68319 9.63837 3.36632 9.44983C3.12787 10.2584 2.99991 11.1142 2.99991 12C2.99991 13.9462 3.61763 15.748 4.6677 17.2203C6.83038 14.1875 10.3685 12.4987 13.9964 12.6219ZM6.047 18.7502C7.77258 16.059 10.7714 14.5382 13.8585 14.6191C13.723 15.3586 13.4919 16.093 13.1594 16.8062C12.3777 18.4825 11.1453 19.805 9.67385 20.6965C8.31043 20.3328 7.07441 19.6569 6.047 18.7502ZM11.9999 3C13.7846 3 15.4479 3.51946 16.847 4.41543C16.2113 5.54838 15.3593 6.4961 14.368 7.23057C13.3472 5.57072 11.8752 4.16433 10.027 3.21692C10.6619 3.07492 11.3222 3 11.9999 3ZM8.80619 4.84582C10.4462 5.61056 11.7474 6.80659 12.6379 8.23588C10.3464 9.24654 7.64722 9.30095 5.19906 8.15936C4.83384 7.98905 4.48541 7.79735 4.15458 7.58645C4.91365 6.24006 6.00929 5.10867 7.32734 4.30645C7.82672 4.44058 8.32138 4.61975 8.80619 4.84582Z'
-									fill='currentColor'
-								/>
-							</svg>
-						</a>
-
-						<a
-							href='#'
-							class='border-b-2 border-transparent hover:text-gray-800 transition-colors duration-200 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6'>
-							<svg
-								class='w-5 h-5 fill-current'
-								viewBox='0 0 24 24'
-								xmlns='http://www.w3.org/2000/svg'>
-								<path d='m.75 19h7.092c4.552 0 6.131-6.037 2.107-8.203 2.701-2.354 1.029-6.797-2.595-6.797h-6.604c-.414 0-.75.336-.75.75v13.5c0 .414.336.75.75.75zm.75-13.5h5.854c3.211 0 3.215 4.768 0 4.768h-5.854zm0 6.268h6.342c3.861 0 3.861 5.732 0 5.732h-6.342z' />
-								<path d='m18.374 7.857c-3.259 0-5.755 2.888-5.635 5.159-.247 3.28 2.397 5.984 5.635 5.984 2.012 0 3.888-1.065 4.895-2.781.503-.857-.791-1.613-1.293-.76-.739 1.259-2.12 2.041-3.602 2.041-2.187 0-3.965-1.668-4.125-3.771 1.443.017 4.136-.188 8.987-.033.016 0 .027-.008.042-.008 2-.09-.189-5.831-4.904-5.831zm-3.928 4.298c1.286-3.789 6.718-3.676 7.89.064-4.064.097-6.496-.066-7.89-.064z' />
-								<path d='m21.308 6.464c.993 0 .992-1.5 0-1.5h-5.87c-.993 0-.992 1.5 0 1.5z' />
-							</svg>
-						</a>
-
-						<button onClick={() => handleClick("en")}>English</button>
-						<button onClick={() => handleClick("am")}>Amharic</button>
 					</div>
 				</nav>
 			</div>
